51dce4a22bc4eecf65223d0235bd77bdbc72ccc4
Pamela Ghag <pghag@ucsd.edu>
2021-01-21 12:30:51 -0800

    Interactive web

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,11 +8,32 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
-function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+ function initializePage() {
+ 	$("#testjs").click(function(e) {
+ 		$('.jumbotron h1').text("Javascript is connected");
+ 			$("#testjs").text("Please wait...");
+ 			$(".jumbotron p").toggleClass("active");
+ 		});
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
+
+function projectClick(e){
+	console.log("project clicked");
+	e.preventDefault();
+	$(this).css("background-color","#7fff00");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+} else {
+		description.fadeToggle();
+
+	}
+}
+
+
+
